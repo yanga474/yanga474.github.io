fee571e02bb6d885830829518c92b04844040d28
yanga474 <angelayang@Angela-Yang-2.local>
2017-02-22 13:52:30 -0500

    commit

---
+++ ci2017/madlibs/mad.js
@@ -0,0 +1,23 @@
+var user_input = prompt("enter a number");
+
+
+
+console.log("i got " + user_input);
+
+
+var answer_paragraph = document.querySelector(".answer");
+
+answer_paragraph.textContent = final_result; 
+
+// multiply_by_three(user_input);
+
+
+// function multiply_by_three(thing_to_multiply) {
+// 	var final_result = thing_to_multiply * 3;
+
+// 	var output = "i got: " + thing_to_multiply + " and the result is";
+
+
+
+
+// }
