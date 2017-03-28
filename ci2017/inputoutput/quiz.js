e64d1943f96ef8a3e1dde93f423fc36b5383e2fd
yanga474 <angelayang@Angela-Yang-2.local>
2017-03-28 04:07:36 -0400

    commit

---
+++ ci2017/inputoutput/quiz.js
@@ -0,0 +1,21 @@
+function check(){
+
+var question1 = document.quiz.question1.value;
+var question2 = document.quiz.question2.value;
+var question3 = document.quiz.question3.value;
+var correct = 0; 
+
+if (question 1 == "Canberra") {
+	correct++;
+}
+if (question 1 == "Canberra") {
+	correct++;
+}
+if (question 1 == "Canberra") {
+	correct++;
+}
+	
+document.getElementById("after_submit").style.visibility = "visible";
+document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
+
+}
