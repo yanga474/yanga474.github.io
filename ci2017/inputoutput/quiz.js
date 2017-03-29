4f354205c4f36d783d843dbc7892bf4ce589a573
yanga474 <angelayang@Angela-Yang-2.wireless.newschool.edu>
2017-03-28 22:02:50 -0400

    commit

--- ci2017/inputoutput/quiz.js
+++ ci2017/inputoutput/quiz.js
@@ -1,21 +1,43 @@
-function check(){
+// function check(){
 
-var question1 = document.quiz.question1.value;
-var question2 = document.quiz.question2.value;
-var question3 = document.quiz.question3.value;
-var correct = 0; 
+// var question1 = document.quiz.question1.value;
+// var question2 = document.quiz.question2.value;
+// var question3 = document.quiz.question3.value;
+// var question4 = document.quiz.question4.value;
 
-if (question 1 == "Canberra") {
-	correct++;
-}
-if (question 1 == "Canberra") {
-	correct++;
-}
-if (question 1 == "Canberra") {
-	correct++;
-}
+// var correct = 0; 
+
+// if (question1 == "Canberra") {
+// 	correct++;
+// }
+
+// if (question2 == "Finland") {
+// 	correct++;
+// }
+
+// if (question3 == "Ottawa") {
+// 	correct++;
+// }
+
+// if (question4 == "Hawaii") {
+// 	correct++;
+// }
 	
-document.getElementById("after_submit").style.visibility = "visible";
-document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
+// document.getElementById("after_submit").style.visibility = "visible";
+// document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
 
+// }
+
+
+var myinputs = document.querySelectorAll("input");
+console.log (myinputs)
+
+for (var i = 0; i < myinputs.length; i++) {
+	console.log(i);
+	myinputs[i].addEventListener("click", onInputClick);
+
+}
+
+function onInputClick(e) {
+	e.target.
 }
