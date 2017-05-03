3c80d9a95bbe3b3893b87937df925482b5592cda
yanga474 <yanga474@newschool.edu>
2017-05-03 14:38:42 -0400

    commit

--- ci2017/final/data.js
+++ ci2017/final/data.js
@@ -1,9 +1,154 @@
 var audio = [
 {
-"date": "04/08/17"
-"time": "",
-"timeofday": "noon",
-"audioSrc": "audio/......mp3"
+"date": "04/08/17",
+"time": "12:19AM",
+"timeofday": "night",
+"audioSrc": "audio/04-08-12.19AM"
 },
-{}
+
+{
+"date": "04/09/17",
+"time": "8:23AM",
+"timeofday": "morning",
+"audioSrc": "audio/04-09-8.23AM"
+},
+
+{
+"date": "04/10/17",
+"time": "7:01PM",
+"timeofday": "night",
+"audioSrc": "audio/04-10-7.01PM"
+},
+
+{
+"date": "04/11/17",
+"time": "4:06PM",
+"timeofday": "afternoon",
+"audioSrc": "audio/04-11-4.06PM"
+},
+
+{
+"date": "04/12/17",
+"time": "11:43AM",
+"timeofday": "morning",
+"audioSrc": "audio/04-12-11.43AM"
+},
+
+{
+"date": "04/13/17",
+"time": "12:15PM",
+"timeofday": "afternoon",
+"audioSrc": "audio/04-13-12.15PM"
+},
+
+{
+"date": "04/14/17",
+"time": "11:48PM",
+"timeofday": "night",
+"audioSrc": "audio/04-14-11.48PM"
+},
+
+{
+"date": "04/15/17",
+"time": "2:32AM",
+"timeofday": "morning",
+"audioSrc": "audio/04-15-2.32AM"
+},
+
+{
+"date": "04/16/17",
+"time": "1:03PM",
+"timeofday": "afternoon",
+"audioSrc": "audio/04-16-1.03PM"
+},
+
+{
+"date": "04/17/17",
+"time": "5:14PM",
+"timeofday": "afternoon",
+"audioSrc": "audio/04-17-5.14PM"
+},
+
+{
+"date": "04/18/17",
+"time": "10:30AM",
+"timeofday": "morning",
+"audioSrc": "audio/04-18-10.30AM"
+},
+
+{
+"date": "04/19/17",
+"time": "2:26PM",
+"timeofday": "afternoon",
+"audioSrc": "audio/04-19-2.26PM"
+},
+
+{
+"date": "04/20/17",
+"time": "10:38AM",
+"timeofday": "morning",
+"audioSrc": "audio/04-20-10.38AM"
+},
+
+{
+"date": "04/21/17",
+"time": "9:03PM",
+"timeofday": "night",
+"audioSrc": "audio/04-21-9.03PM"
+},
+
+{
+"date": "04/22/17",
+"time": "10:00PM",
+"timeofday": "night",
+"audioSrc": "audio/04-22-10PM"
+},
+
+{
+"date": "04/23/17",
+"time": "9:42AM",
+"timeofday": "morning",
+"audioSrc": "audio/04-23-9.42AM"
+},
+
+{
+"date": "04/24/17",
+"time": "10:04PM",
+"timeofday": "night",
+"audioSrc": "audio/04-24-10.04PM"
+},
+
+{
+"date": "04/25/17",
+"time": "9:50PM",
+"timeofday": "night",
+"audioSrc": "audio/04-25-9.50PM"
+},
+
+{
+"date": "04/26/17",
+"time": "9:41AM",
+"timeofday": "morning",
+"audioSrc": "audio/04-26-9.41AM"
+},
+
+{
+"date": "04/27/17",
+"time": "3.38PM",
+"timeofday": "afternoon",
+"audioSrc": "audio/04-27-3.38PM"
+}
 ];
+
+
+
+
+
+
+
+
+
+
+
+
+
--- ci2017/final/main.js
+++ ci2017/final/main.js
@@ -1,17 +1,69 @@
 var morning = document.querySelector("#morning");
-console.log(morning)
-morning.addEventListener("click", onMorningClick);
-
-function onMorningClick(){
-	console.log("MORNING");
-	//holder.innerHTML = "";
-}
-
+// console.log(morning)
 var afternoon = document.querySelector("#afternoon");
-console.log(afternoon)
-afternoon.addEventListener("click", onAfternoonClick);
+// console.log(afternoon)
+var night = document.querySelector("#night");
 
-function onAfternoonClick(){
-	console.log("AFTERNOOON!!!!!")
+var home = document.getElementById("home");
+
+var h1 = document.querySelector("h1");
+
+
+
+afternoon.addEventListener("click", goWhite);
+morning.addEventListener("click", goWhite);
+night.addEventListener ("click", goWhite);
+
+home.addEventListener("click", function(e){
+	document.body.style.backgroundColor = "black";
+	h1.style.color = "white";
+})
+
+
+
+function goWhite(e){
+	var timeofday = (e.currentTarget.getAttribute("id"));
+
+	var filtered = _.filter(audio, function(a) {
+		return a.timeofday == timeofday
+	});
+
+	console.log(filtered);
+
+	makeHTML(filtered);
 	
+	document.body.style.backgroundColor = "white";
+	h1.style.color = "black";
+
 }
+
+function makeHTML (stufftoget){
+
+	var holder = document.getElementById("holder");
+	holder.innerHTML = "";
+
+	for (var i = 0; i < stufftoget.length; i++) {
+		var audio = stufftoget[i];
+
+		var diffAudio = document.createElement("div");
+
+		var audiotag = document.createElement("audio");
+		audiotag.src = audio.audioSrc + ".mp3";
+		audiotag.controls = "controls";
+		audiotag.loop = true;
+		diffAudio.appendChild(audiotag);
+
+		// diffAudio.innerText = audio.audioSrc;
+		holder.appendChild(diffAudio);
+
+	}
+
+}
+
+
+	
+
+
+
+
+
